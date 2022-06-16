cc83817b12a2ce6d3b15f67b4c4821defdb2258c
ojocc <ccparrow@qq.com>
2022-06-16 15:54:01 +0800

    mac

--- Source/Visualization/MAC/mac.cpp
+++ Source/Visualization/MAC/mac.cpp
@@ -2,7 +2,7 @@
 
 
 #include "mac.h"
-#include "mac.c"
+#include <sstream>
 
 
 // Sets default values
@@ -27,19 +27,25 @@ void Amac::Tick(float DeltaTime)
 
 }
 
-void Amac::getMac(FString mac, FString data, FString& retData)
+FString Amac::encryptMAC(FString mac)
 {
-	// unsigned char* _mac();
-	// std::string smac = TCHAR_TO_UTF8(*mac);
-	// smac.append(reinterpret_cast<const char*>(_mac()));
+	unsigned char _cMac[8] = {0x4f, 0xf6, 0x3e, 0x5d, 0x02, 0xff, 0xca, 0x1b}; //密钥的定义
+	unsigned char _cData[19] = {0x00, 0x00, 0x12, 0x34, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x20, 0x12, 0x12, 0x07, 0x16, 0x41, 0x00,0x80}; //加密数据的定义,加密数据定长.
+	unsigned char _cOutData[4];
+
+	const std::string& ustrPath(TCHAR_TO_UTF8(*mac));
+	unsigned char cMac[8];
+	memcpy(cMac,ustrPath.data(),ustrPath.size());
 	
-	// unsigned char* _mac= TCHAR_TO_ANSI(*mac);
-	// unsigned char* _data= TCHAR_TO_ANSI(*data);
-	// unsigned char* _retData= TCHAR_TO_ANSI(*retData);
-	//
-	// MacArithmetic(_mac,_data,19,_retData);
+	MacArithmetic(_cMac, _cData, 19, _cOutData);
+
+	std::string sRetData = "";
+	for (int i = 0; i < 4; i++)
+		sRetData += _cOutData[i];
+
+	FString ustr = UTF8_TO_TCHAR(sRetData.c_str());
 	
-	MacArithmetic(_mac,_data,19,_retData);
-	
+	return ustr;
 }
+
 
